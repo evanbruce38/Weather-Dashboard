2d0b90d60a5abb5a00a8649b080e0ee970b40f32
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-07-18 19:59:36 -0400

    edit styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,38 +2,39 @@
 var apiKey = "1b18ce13c84e21faafb19c931bb29331";
 var savedSearches = [];
 
-// list of previously searched cities
+// make list of previously searched cities
 var searchHistoryList = function(cityName) {
     $('.past-search:contains("' + cityName + '")').remove();
 
-    // create an entry with city name
+    // create entry with city name
     var searchHistoryEntry = $("<p>");
     searchHistoryEntry.addClass("past-search");
     searchHistoryEntry.text(cityName);
 
     // create container for entry
-    var searchHistoryContainer = $("<div>");
-    searchHistoryContainer.addClass("past-search-container");
+    var searchEntryContainer = $("<div>");
+    searchEntryContainer.addClass("past-search-container");
 
     // append entry to container
-    searchHistoryContainer.append(searchHistoryEntry);
+    searchEntryContainer.append(searchHistoryEntry);
 
     // append entry container to search history container
     var searchHistoryContainerEl = $("#search-history-container");
     searchHistoryContainerEl.append(searchEntryContainer);
 
-    if(savedSearches.length > 0) {
-        // update savedSearches with previously saved searches
+    if (savedSearches.length > 0){
+        // update savedSearches array with previously saved searches
         var previousSavedSearches = localStorage.getItem("savedSearches");
         savedSearches = JSON.parse(previousSavedSearches);
     }
 
     // add city name to array of saved searches
     savedSearches.push(cityName);
-    localStorage.setItem("savedSearches". JSON.stringify(savedSearches));
+    localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
 
     // reset search input
     $("#search-input").val("");
+
 };
 
 // load saved search history entries into search history container
